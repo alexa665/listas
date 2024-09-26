7ff6523da40d7bdff1889eb047dedeafeaac8e7a
alexa665 <a.23308051281205@cbtis128.edu.mx>
2024-09-25 20:34:58 -0600

    Add files via upload

---
+++ lis.py
@@ -0,0 +1,39 @@
+print(" ")
+print("alexa guadalupe ramirez manzo")
+print(" ")
+#Practica 8  donde usamos listas
+thislist = ["mapa", "mochila", "lupa"] #variable y la lista de tres elementos
+print(thislist) #impresion de variable
+
+#cuando usamos (len( )) te despliega
+#el numero de elementos de la lista
+thislist2 = ["mapa", "mochila", "lupa"] #variable y lista de elementos 
+print(len(thislist)) #impresion de lista
+
+
+#Separa con un espacio
+print(" ")
+#Da valores a varias listas
+list1 = ["mapa", "mochila", "lupa"] #variable y lista de tres elementos
+list2 = [1, 5, 7, 9, 3] #variable y los lugares de las listas
+list3 = [True, False, False] #variable de verdadero o falso
+list4 = ["abc", 34, True, 40, "male"] #variable y lista
+
+#Muestra los miembros de cada lista
+print(list1) #impresion de listas
+print(list2) #impresion de listas
+print(list3) #impresion de listas
+print(list4) #impresion de listas
+
+
+print(" ") #Separa con un espacio
+
+thislist = ["mapa", "mochila", "lupa"] #variable y lista de elementos
+
+#Muestra el miembro 1 y comienza en Cero 0
+print(thislist[1]) #impresion de listas
+
+#Muestra el ultimo miembro de la lisa
+print(thislist[-3]) #impresion de listas
+
+print(" ") #Separa con un espacio
